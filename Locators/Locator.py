6e0f8f61690254fc5dd9244ac7184b8d9817e242
Shubham0551 <shubhamsingh.as1@krishnacollege.ac.in>
2022-11-02 09:19:39 +0530

    adding Locators

---
+++ Locators/Locator.py
@@ -0,0 +1,7 @@
+city = "//input[@id='city']"
+search = "//button[@name='search']"
+npi = "/html[1]/body[1]/app-root[1]/main[1]/div[1]/app-results[1]/table[1]/tbody[1]/tr[1]/td[1]/button[1]"
+name_link = "/html[1]/body[1]/app-root[1]/main[1]/div[1]/app-provider-view[1]/div[3]/blockquote[1]/p[1]"
+npi_link = "/html[1]/body[1]/app-root[1]/main[1]/div[1]/app-provider-view[1]/div[3]/table[1]/tbody[1]/tr[1]/td[2]"
+address_link = "/html[1]/body[1]/app-root[1]/main[1]/div[1]/app-provider-view[1]/div[3]/table[2]/tbody[1]/tr[6]/td[2]"
+taxnomoy = "/html[1]/body[1]/app-root[1]/main[1]/div[1]/app-provider-view[1]/div[3]/table[2]/tbody[1]/tr[11]/td[2]/table[1]/tbody[1]/tr[1]/td[2]"
